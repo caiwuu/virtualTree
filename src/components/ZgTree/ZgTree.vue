9421031da646f38862910a87bf13a205c036f7b7
hspcadmin <1334416530@qq.com>
2022-09-29 10:29:34 +0800

    feat: tree

--- src/components/ZgTree/ZgTree.vue
+++ src/components/ZgTree/ZgTree.vue
@@ -1,39 +1,47 @@
 <template>
-  <div>
-    <div class="zg-tree" v-for="(item,index) in data" :key="index"
-      :style="{marginLeft:(item.level * levelDistance)+'px'}" @click="collapseChange(item,index)">
-      <div class="zg-triangle">
-        <div class="zg-triangle-right" v-if="item.collapsed && !item.isLeaf"></div>
-        <div class="zg-triangle-bottom" v-if="!item.collapsed && !item.isLeaf"></div>
-      </div>
-      <div class="zg-content">
-        {{item.name}}
-      </div>
+  <div class="zg-tree"
+       v-for="(item,index) in data"
+       :key="index"
+       :style="{paddingLeft:(item.level * levelDistance)+'px'}"
+       @click="collapseChange(item,index)">
+    <div class="zg-triangle">
+      <div class="zg-triangle-right" v-if="item.collapsed && !item.isLeaf"></div>
+      <div class="zg-triangle-bottom" v-if="!item.collapsed && !item.isLeaf"></div>
+    </div>
+    <label class="zg-checkbox" @click.stop="selectChange(item,index)">
+      <span :class="{
+        'zg-checkbox__input': true,
+        'is-indeterminate': item.checkType === 1,
+        'is-checked': item.checkType === 2
+      }">
+        <span class="zg-checkbox__inner"></span>
+      </span>
+    </label>
+    <div class="zg-content">
+      {{item.name}}
     </div>
   </div>
 </template>
 
 <script>
-// import { data } from '@/utils/data'
-import { data } from '@/mock'
+import { data } from '@/utils/data'
+// import { data } from '@/mock'
 export default {
   components: {},
   props: {
     levelDistance: {
       type: [Number],
       default: 10,
-    },
-    top: {
-      type: [String, Number],
-      top: 0,
-    },
+    }
   },
   data() {
     return {
-      data: data.list
+      data: data
     };
   },
   computed() {
+  },
+  created() {
   },
   methods: {
     /**
@@ -47,17 +55,59 @@ export default {
     /**
      * tree行点击事件
      * @param id
-     * @returns {boolean}
+     * @param index
      */
     collapseChange(item, index) {
       item.collapsed = !item.collapsed
       this.$emit('collapseChange')
+    },
+    /**
+     * tree行点击事件
+     * @param level
+     * @param id
+     * @param pid
+     * @returns {boolean}
+     */
+    checkAll({level, id, pid}) {
+      this.data.forEach((e, i)=>{
+        if(id !== e.id){
+          if(e.level > level && e.pid === id){
+            e.checkType = 2;
+            if(!e.isLeaf) {
+              this.checkAll({level:++level, id:e.id, pid:e.pid})
+            }
+          }
+        }
+      })
+    },
+    /**
+     * check选中事件
+     * @param item
+     * @param index
+     */
+    selectChange(item, index) {
+      const type = {
+        0: ()=> {
+          item.checkType = 2;
+
+        },
+        1: ()=> {
+          item.checkType = 2;
+
+        },
+        2: ()=> {
+          item.checkType = 0;
+        }
+      }
+      type[item.checkType] && type[item.checkType]()
+      this.$emit('selectChange')
     },
   },
 };
 </script>
 
 <style scoped>
+
 ul {
   padding: 0;
   margin: 0;
@@ -109,4 +159,83 @@ li {
   border-right: 4px solid transparent;
   border-bottom: 0 solid transparent;
 }
+
+.zg-checkbox {
+  color: #606266;
+  font-weight: 500;
+  font-size: 14px;
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+  white-space: nowrap;
+  user-select: none;
+  vertical-align: middle;
+  margin-right: 8px;
+}
+
+.zg-checkbox__input {
+  white-space: nowrap;
+  cursor: pointer;
+  outline: none;
+  display: inline-block;
+  line-height: 1;
+  position: relative;
+  vertical-align: middle;
+}
+
+.zg-checkbox__inner {
+  display: inline-block;
+  position: relative;
+  border: 1px solid #dcdfe6;
+  border-radius: 2px;
+  box-sizing: border-box;
+  width: 14px;
+  height: 14px;
+  background-color: #fff;
+  z-index: 1;
+  transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);
+}
+
+.zg-checkbox__inner:after {
+  box-sizing: content-box;
+  content: "";
+  border: 1px solid #fff;
+  border-left: 0;
+  border-top: 0;
+  height: 8px;
+  left: 4px;
+  position: absolute;
+  top: 1px;
+  transform: rotate(45deg) scaleY(0);
+  width: 3px;
+  transition: transform .15s ease-in .05s;
+  transform-origin: center;
+}
+
+.zg-checkbox__input.is-checked .zg-checkbox__inner {
+  background-color: #409eff;
+  border-color: #409eff;
+}
+
+.zg-checkbox__input.is-checked .zg-checkbox__inner:after {
+  transform: rotate(45deg) scaleY(1);
+}
+
+.zg-checkbox__input.is-indeterminate .zg-checkbox__inner {
+  background-color: #409eff;
+  border-color: #409eff;
+}
+
+.zg-checkbox__input.is-indeterminate .zg-checkbox__inner:before {
+  content: "";
+  position: absolute;
+  display: block;
+  background-color: #fff;
+  height: 2px;
+  transform: scale(.5);
+  left: 0;
+  right: 0;
+  top: 5px;
+}
+
 </style>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,11 +1,7 @@
 export const data = [
-    {id:1, pid:0, level:1, name: "父节点1", collapsed: false, isLeaf: false, data: {}},
-    {id:11, pid:1,level:2, name: "子节点1", collapsed: false, isLeaf: false, data: {}},
-    {id:111, pid:11,level:3, name: "子节点1", collapsed: false, isLeaf: true, data: {}},
-    {id:12, pid:1,level:2, name: "123", collapsed: true,isLeaf: true, data: {}},
-];
-
-export const data2 = [
-    {id:1, pid:0, level:1, name: "父节点1", collapsed: true, isLeaf: false, data: {}},
-    {id:12, pid:1,level:2, name: "123", collapsed: true, isLeaf: true, data: {}},
+    {id:1, pid:0, level:0, checkType:0, name: "父节点1", collapsed: false, isLeaf: false, data: {}},
+    {id:11, pid:1,level:1, checkType:1, name: "子节点11", collapsed: false, isLeaf: false, data: {}},
+    {id:111, pid:11,level:2, checkType:0, name: "子节点111", collapsed: false, isLeaf: true, data: {}},
+    {id:1111, pid:11,level:2, checkType:2, name: "子节点1111", collapsed: false, isLeaf: true, data: {}},
+    {id:12, pid:1,level:1, checkType:0, name: "子节点12", collapsed: true,isLeaf: true, data: {}},
 ];
